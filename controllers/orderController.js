63206dd38bc834a9f60295f75c9a318b3cb3d2ce
smsakawat <sakawat602872@gmail.com>
2021-12-25 22:46:39 +0600

    get,update & delete order APIs done

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,7 @@
 const Order = require("../models/orderModel");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const ErrorHandler = require("../utils/errorHandler");
+const Product = require("../models/productModel");
 
 // Create New Order
 exports.newOrder = catchAsyncErrors(async (req, res, next) => {
@@ -27,5 +28,96 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
   res.status(201).json({
     success: true,
     order,
+  });
+});
+
+// Get Single Order (Admin)
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+  // so we'll use populate method of mongoose here to get the user's (orderer's) name and email by his id
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(
+      new ErrorHandler(`Order not found with id:${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+// Get My Orders For User
+exports.myOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+});
+
+// Get All Orders(Admin)
+exports.getAllOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+
+  // count total amount for dashboaed
+  let totalAmount = 0;
+  orders.forEach((order) => {
+    totalAmount += order.totalPrice;
+  });
+  res.status(200).json({
+    success: true,
+    totalAmount,
+    orders,
+  });
+});
+
+// Update Order (Admin)
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+  if (!order) {
+    return next(new ErrorHandler(`Order not found with this Id`));
+  }
+
+  if (order.orderStatus === "Delivered") {
+    return next(new ErrorHandler("You have already delivered this order"), 400);
+  }
+  order.orderStatus = req.body.status;
+  // handle proeuct stock after deliver products
+  order.orderItems.forEach(async (order) => {
+    await updateStock(order.product, order.quantity);
+  });
+
+  if (req.body.status === "Delivered") {
+    order.deliveredAt = Date.now();
+  }
+  order.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+// update stock  after deleviver
+async function updateStock(id, quantity) {
+  const product = await Product.findById(id);
+
+  product.stock = product.stock - quantity;
+  await product.save({ validateBeforeSave: false });
+}
+
+// Delete An Order (Admin)
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this id"));
+  }
+  await order.remove();
+  res.status(200).json({
+    success: true,
+    message: "Order deleted successfully",
   });
 });
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,9 +1,29 @@
 const express = require("express");
-const { newOrder } = require("../controllers/orderController");
-const { isAuthenticatedUser } = require("../middleware/auth");
+const {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+  getAllOrders,
+  updateOrder,
+  deleteOrder,
+} = require("../controllers/orderController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
-// New Order Route4
+// New Order Route
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
+// Get Single Order Route
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+// Get my orders route for user
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+// Get all orders route for admin
+router
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+// update and delete order routes for admin
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
 
 module.exports = router;
